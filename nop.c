805686e7669f541fc122b031527248be63ed7655
https://github.com/kelvintechsolutions <kevohmutugi51@gmail.com>
2024-01-19 05:17:05 -0800

    more task

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing.
+  *@head:it has stack head.
+  *@counter:it is line_number.
+  *Return:it has no return.
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
