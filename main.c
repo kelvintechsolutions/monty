ce480926a59ce2c70f25585836c8d0d0ebd346d1
Brianoysters <144411003+Brianoysters@users.noreply.github.com>
2024-01-20 11:25:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "monty.h"
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
-* main -it is monty code interpreter.
+* main -this is a function that interprets monty code.
 * @argc:it is a number of arguments.
 * @argv:it is monty file location.
 * Return: 0 if success.
