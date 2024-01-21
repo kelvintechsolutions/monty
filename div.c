c0b3ea220f8e0b33b150e334adcececf8851b233
Brianoysters <brianoysters@gmail.com>
2024-01-20 20:36:32 -0800

    update

--- div.c
+++ div.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 /**
- * f_div -this function divides the top two elements of the stack.
+ * f_div -function divides the top two elements of the stack.
  * @head:it is stack head.
  * @counter:it is a line_number.
  * Return:it is  no return.
